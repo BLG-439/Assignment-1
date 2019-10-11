ed48d576b9f0234debfcd1384004a2ad16f1b08e
teo-1 <mateo.nr1@hotmail.com>
2019-10-11 16:42:09 +0300

    website 1 (Log in)

---
+++ READ_ME.txt
@@ -0,0 +1,3 @@
+- To enable audio autoplay feature:
+    1. Go to chrome://flags/#autoplay-policy
+    2. On "Autoplay policy" section select the "No user gesture is required" option
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import SignInForm
 
 app=Flask(__name__)
+app.config['SECRET_KEY']='f1cc00c7d395eab3b4c3'
 
 @app.route('/')
 def index():
-  kot="Plot"
-  return render_template('index.html', kot=kot)
+  form = SignInForm()
+  return render_template('signin.html', Hello="static/audio/hello.mp3", Signin_email="static/audio/sign_in1.mp3", Signin_password="static/audio/sign_in2.mp3", form=form)
+
+@app.route('/kot')
+def plot():
+  return render_template('kot.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
